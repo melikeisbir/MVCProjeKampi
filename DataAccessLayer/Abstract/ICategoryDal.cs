b5a391a2beb8a0916cc91c19cb292d8154e6bd49
Melike İsbir <melikeisbr@gmail.com>
2023-09-11 22:56:35 +0300

    abstract and interface components

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        //CRUD operasyonunu metot olarak tanımlayacağız
+        //Metotun tipi ismi ();  --->Type Name();
+        List<Category> List(); //türü ve ismi list olan bir metot tanımlandı
+        void Insert(Category p); //ekleme işlemi
+        void Update(Category p);
+        void Delete(Category p);
+
+    }
+}
