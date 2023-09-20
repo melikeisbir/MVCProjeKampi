34c2b84deec0f0646215d114c52aa4ac91512434
Melike İsbir <melikeisbr@gmail.com>
2023-09-20 22:36:32 +0300

    writer tasks

---
+++ MVCProjeKampi/Controllers/WriterController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjeKampi.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager vm = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var WriterValues=vm.GetList();
+            return View(WriterValues);
+        }
+    }
+}
