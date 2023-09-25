a32f930a43b2f6398bb733625e303246aff90a92
Melike İsbir <melikeisbr@gmail.com>
2023-09-25 23:38:28 +0300

    Heading Tasks, Using DropDownList and Relationships in Title Tasks

---
+++ MVCProjeKampi/Controllers/HeadingController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjeKampi.Controllers
+{
+    public class HeadingController : Controller
+    {
+        // GET: Heading
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valueacategory = (from x in cm.GetList()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value=x.CategoryID.ToString(),
+                                                   }
+                                                   ).ToList(); //ilgili tablonun bütün değerlerini alacak -- verileri dropdownlist aracılığıyla listeleyecek
+            ViewBag.vlc=valueacategory; //view tarafına taşıma
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate= DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.HeadingAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
