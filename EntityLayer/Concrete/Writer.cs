5a94045b2fbf689a0b307466547515ded6175d9d
Melike İsbir <melikeisbr@gmail.com>
2023-09-26 22:45:59 +0300

    the process of writer and category handling in headings, heading tasks, heading formatting

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -15,7 +15,7 @@ namespace EntityLayer.Concrete
         public string WriterName { get; set; }
         [StringLength(500)]
         public string WriterSurName { get; set; }
-        [StringLength(100)]
+        [StringLength(250)]
         public string WriterImage { get; set; }
         [StringLength(100)]
         public string WriterAbout { get; set; }
