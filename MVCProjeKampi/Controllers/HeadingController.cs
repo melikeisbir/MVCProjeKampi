65259bae2faf19e135aee2544dd8e3ce44957b15
Melike İsbir <melikeisbr@gmail.com>
2023-09-30 21:03:09 +0300

    popup usage, about page

--- MVCProjeKampi/Controllers/HeadingController.cs
+++ MVCProjeKampi/Controllers/HeadingController.cs
@@ -71,6 +71,7 @@ namespace MVCProjeKampi.Controllers
         public ActionResult DeleteHeading(int id)
         {
             var HeadingValue=hm.GetByID(id);
+            HeadingValue.HeadingStatus = false;
             hm.HeadingDelete(HeadingValue);
             return RedirectToAction("Index");
         }
