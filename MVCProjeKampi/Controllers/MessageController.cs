2345ec10b26889d951cfdd971d86c6c2e3b5abd1
Melike İsbir <melikeisbr@gmail.com>
2023-10-09 22:25:49 +0300

    the completion of message processing

--- MVCProjeKampi/Controllers/MessageController.cs
+++ MVCProjeKampi/Controllers/MessageController.cs
@@ -2,6 +2,7 @@
 using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,21 +14,26 @@ namespace MVCProjeKampi.Controllers
     public class MessageController : Controller
     {
         // GET: Message
-        MessageManager cm = new MessageManager(new EfMessageDal());
-        //MessageValidator messageValidator = new MessageValidator();
+        MessageManager mm = new MessageManager(new EfMessageDal());
+        MessageValidator messagevalidator = new MessageValidator();
         public ActionResult Inbox()
         {
-            var messagelist = cm.GetListInbox();
+            var messagelist = mm.GetListInbox();
             return View(messagelist);
         }
         public ActionResult Sendbox()
         {
-            var messagelist = cm.GetListSendbox();
+            var messagelist = mm.GetListSendbox();
             return View(messagelist);
         }
         public ActionResult GetInboxMessageDetails(int id)
         {
-            var values = cm.GetByID(id);
+            var values = mm.GetByID(id);
+            return View(values);
+        }
+        public ActionResult GetSendboxMessageDetails(int id)
+        {
+            var values = mm.GetByID(id);
             return View(values);
         }
         [HttpGet]
@@ -38,6 +44,20 @@ namespace MVCProjeKampi.Controllers
         [HttpPost]
         public ActionResult NewMessage(Message p)
         {
+            ValidationResult results = messagevalidator.Validate(p);
+            if (results.IsValid)
+            {
+                p.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                mm.MessageAdd(p);
+                return RedirectToAction("Sendbox");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View(p);
         }
     }
