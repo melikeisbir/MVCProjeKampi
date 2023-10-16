a67022a854a757ee99185ab29101754333d2e838
Melike İsbir <melikeisbr@gmail.com>
2023-10-16 22:24:18 +0300

    roles

--- MVCProjeKampi/Controllers/AdminCategoryController.cs
+++ MVCProjeKampi/Controllers/AdminCategoryController.cs
@@ -14,7 +14,7 @@ namespace MVCProjeKampi.Controllers
     public class AdminCategoryController : Controller
     {
         CategoryManager cm = new CategoryManager(new EfCategoryDal());
-        [Authorize]
+        [Authorize(Roles="B")]
         public ActionResult Index()
         {
             var categoryvalues = cm.GetList();
