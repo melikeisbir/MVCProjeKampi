8ff52a86f45b34e7423d796400a1822f3b07b9f6
Melike İsbir <melikeisbr@gmail.com>
2023-12-03 17:54:37 +0300

    Authorization(new Admin)

---
+++ MVCProjeKampi/Controllers/AuthorizationController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjeKampi.Controllers
+{
+    [AllowAnonymous]
+    public class AuthorizationController : Controller
+    {
+        AdminManager adminmanager = new AdminManager(new EfAdminDal());
+        public ActionResult Index()
+        {
+            var adminvalues = adminmanager.GetList();
+            return View(adminvalues);
+        }
+    }
+}
